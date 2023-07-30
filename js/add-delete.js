29ecbb79245fb95c8ef0e1ad248b7eaff69eabef
Mohammad_Fazeli <mohammadfazeli.developer@gmail.com>
2023-07-30 14:59:02 +0330

    feat/mohammad-fazeli/product-create

---
+++ js/add-delete.js
@@ -0,0 +1,15 @@
+const product = {
+    name:"iPhone 13",
+    price:46000000,
+    size: size,
+    color:"Green",
+    idProduct:10002023,
+    stock:true,
+    
+    addToCart:function () {
+    },
+    
+    removeFromCart:function () {
+    },
+
+};
